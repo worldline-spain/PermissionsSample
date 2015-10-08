ed90ede613902f5a4747460a928973aa464d1764
alorma <bernatbor15@gmail.com>
2015-10-08 17:30:26 +0200

    Add Broadcast not granted snackbar

--- app/src/main/java/com/worldline/permissions/sample/MainActivity.java
+++ app/src/main/java/com/worldline/permissions/sample/MainActivity.java
@@ -5,13 +5,14 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.ServiceInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -135,7 +136,10 @@ public class MainActivity extends AppCompatActivity {
     private void sendBroadcastEvent() {
         Intent intent = new Intent();
         intent.setAction(BROADCAST_ACTION);
-
         sendBroadcast(intent);
+
+        if (ContextCompat.checkSelfPermission(this, "com.worldline.permissions.ADD_INFO") != PackageManager.PERMISSION_GRANTED) {
+            Snackbar.make(content, "Without ADD_INFO permission, broadcast is not delivered", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
